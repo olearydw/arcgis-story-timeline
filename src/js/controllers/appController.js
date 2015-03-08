e90516e268cafbee7e077ab5becf2333f7d47ff8
Dan O'Leary <doleary@esri.com>
2015-03-08 10:27:43 -0400

    adding controller files to tracked file list

---
+++ src/js/controllers/appController.js
@@ -0,0 +1,42 @@
+﻿require([
+  "dojo/ready",
+  "dojo/dom-style",
+  "dojo/dom-attr",
+  "dojo/dom",
+  "dojo/on",
+  "utils/loginmodule",
+  "controllers/viewController",
+  "dojo/domReady!"
+], function (ready, domStyle, domAttr, dom, on, loginmodule, viewController) {
+
+  ready(function () {
+
+    
+    loginmodule.doCheckLoginStatus(function (loggedIn) {
+      viewController.doSetLoginView(loggedIn);
+    });
+    
+    doSetEventHandlers();
+    //domStyle.set("anonymousPanel", "display", "block");
+
+  });
+
+  function doSetEventHandlers() {
+    on(dom.byId("signInBtn"), "click", handleSignIn);
+    on(dom.byId("signOutBtn"), "click", handleSignOut);
+  };
+
+
+  function handleSignIn() {
+    loginmodule.doOauthFormLogin(function (loggedIn) {
+      viewController.doSetLoginView(loggedIn);
+    });
+  };
+
+  function handleSignOut() {
+    loginmodule.doOauthSignOut(function (loggedOut) {
+      window.location.reload();
+    });
+  };
+
+});
---
+++ src/js/controllers/viewController.js
@@ -0,0 +1,54 @@
+﻿define([
+  "dojo/ready",
+  "dojo/dom-style",
+  "dojo/dom-attr",
+  "dojo/dom",
+  "models/modelmodule",
+  "utils/datesmodule"
+], function (ready, domStyle, domAttr, dom, modelmodule, datesmodule) {
+
+  var view = {};
+
+  ready(function () {
+    //console.log('view module ready');
+  });
+
+  view.doSetLoginView = function (loggedIn) {
+    loggedIn ? view.doShowSignedIn() : view.doShowSignedOut();
+  };
+
+  view.doShowSignedIn = function () {
+    domStyle.set("signInBtn", "display", "none");
+    domStyle.set("signOutBtn", "display", "block");
+    domStyle.set("loggedInDiv", "opacity", 1);
+
+    var credObj = modelmodule.getCredentialObj();
+
+    console.log(credObj);
+
+    dojo.byId("serverUrl").innerHTML = "Server: " + credObj.server;
+    dojo.byId("userName").innerHTML = "User Id: <span>" + credObj.userId + "</span>";
+    dojo.byId("createTime").innerHTML = "Created: <span>" + datesmodule.formatDateToUTC(new Date(credObj.creationTime)) + "</span>";
+    dojo.byId("expireTime").innerHTML = "Expires: <span>" + datesmodule.formatDateToUTC(new Date(credObj.expires)) + "</span>";
+    dojo.byId("isSSL").innerHTML = "Is SSL: <span>" + credObj.ssl + "</span>";
+    dojo.byId("myToken").innerHTML = "Token: <span>" + credObj.token + "</span>";
+
+
+
+
+  };
+
+  view.doShowSignedOut = function () {
+    domStyle.set("signInBtn", "display", "block");
+    domStyle.set("signOutBtn", "display", "none");
+    domStyle.set("loggedInDiv", "opacity", 0);
+
+    dojo.byId("serverUrl").innerHTML = "";
+    dojo.byId("userName").innerHTML = "";
+    dojo.byId("createTime").innerHTML = "";
+    dojo.byId("expireTime").innerHTML = "";
+    dojo.byId("myToken").innerHTML = "";
+  };
+
+  return view;
+});
